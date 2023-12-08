b30d1e7b6ee8b90459842f5049cf9e907778f3d5
Juan Najul <juannajulvillegas@gmail.com>
2023-12-08 09:39:08 -0400

    feat: fetch functions to consume api endpoinst

---
+++ src/services/apiFecthServices.js
@@ -0,0 +1,109 @@
+import axios from "axios";
+
+const api = axios.create({
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getData = async (params) => {
+  try {
+    const response = await api.get(params);
+    return response.data;
+  } catch (error) {
+    console.log({ error });
+    return error;
+  }
+};
+
+export const postData = async (params, body = {}) => {
+  try {
+    const response = await api.post(params, body);
+    return response.data;
+  } catch (error) {
+    console.log({ error });
+    return error;
+  }
+};
+
+export const deleteData = async (params) => {
+  try {
+    const response = await api.delete(params);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const updateData = async (params, body) => {
+  try {
+    const response = await api.patch(params, body);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+// Posts
+export const getPosts = async () => {
+  let params = `/api/posts`;
+  return await getData(params);
+};
+
+export const getPostsByCategory = async (categoryId) => {
+  console.log(categoryId);
+  let params = `/api/posts?categoryId=${categoryId}`;
+  return await getData(params);
+};
+
+export const deletePost = async (postId) => {
+  let params = `/api/posts?postId=${postId}`;
+  return await deleteData(params);
+};
+
+export const createPost = async (body) => {
+  let params = `/api/posts`;
+  return await postData(params, body);
+};
+
+export const getPostsByKeyword = async (keyword) => {
+  let params = `/api/posts?keyword=${keyword}`;
+  return await getData(params);
+};
+
+// Categories
+export const getCategories = async () => {
+  let params = `/api/categories`;
+  return await getData(params);
+};
+
+export const createCategory = async (body) => {
+  let params = `/api/categories`;
+  return await postData(params, body);
+};
+
+// Tweets
+export const getTweets = async () => {
+  let params = `/api/tweets`;
+  return await getData(params);
+};
+
+export const getTweetsByCategory = async (categoryId) => {
+  let params = `/api/tweets?categoryId=${categoryId}`;
+  return await getData(params);
+};
+
+export const createTweet = async (body) => {
+  let params = `/api/tweets`;
+  return await postData(params, body);
+};
+
+export const getTweetsByKeyword = async (keyword) => {
+  let params = `/api/tweets?keyword=${keyword}`;
+  return await getData(params);
+};
+
+export const deleteTweet = async (tweetId) => {
+  let params = `/api/tweets?tweetId=${tweetId}`;
+  return await deleteData(params);
+};
