284ecaa28789240398d7b3c40d4b8ab3c16e5b4f
Juan Najul <juannajulvillegas@gmail.com>
2023-12-13 18:18:05 -0400

    fix: Deploy bug fixed

--- src/services/apiFecthServices.js
+++ src/services/apiFecthServices.js
@@ -1,11 +1,6 @@
 import axios from "axios";
-// const API_URL =
-//   process.env.NODE_ENV === "production"
-//     ? `${API_URL}`
-//     : "http://localhost:3000";
 
 const api = axios.create({
-  //baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
