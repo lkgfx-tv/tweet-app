f68c229febbf6869de799479b796521ff766b187
Juan Najul <juannajulvillegas@gmail.com>
2023-12-13 17:52:46 -0400

    fix: comments

--- src/services/apiFecthServices.js
+++ src/services/apiFecthServices.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-const API_URL =
-  process.env.NODE_ENV === "production"
-    ? `${API_URL}`
-    : "http://localhost:3000";
+// const API_URL =
+//   process.env.NODE_ENV === "production"
+//     ? `${API_URL}`
+//     : "http://localhost:3000";
 
 const api = axios.create({
-  baseURL: API_URL,
+  //baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
